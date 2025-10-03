ee79e6e7ee16f6e5f502b0f26b3b069c4aac8801
ysssa-75 <mma23175@multi-school.com>
2025-10-03 14:55:44 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <h1>クイズ</h1>
+    <h1>Question</h1>
     <div className="wrap">
-      
       <App />
-    
     </div>
-    </div>
+  </div>
 );
